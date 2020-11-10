3fbcad9e714cb08b47f118ac0ab534969ef0c53b
bryoz <bryan.mcdowall@gmail.com>
2020-11-10 11:31:57 +0000

    added storybook to React App

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-docs",
+    "@storybook/addon-storysource",
+    "@storybook/addon-a11y",
+    "@storybook/preset-create-react-app"
+  ]
+}
---
+++ src/stories/Button.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// import styles from './button.module.css';
+import styles from './button.module.scss';
+import classNames from 'classnames/bind';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
+  const cx = classNames.bind(styles);
+
+  const classes = cx(
+    "button",
+    primary && "primary",
+    size
+  );
+
+  return (
+    <button
+      type="button"
+      className={classes}
+      style={backgroundColor && { backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary: PropTypes.bool,
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: 'medium',
+  onClick: undefined,
+};
